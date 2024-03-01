bc374cf80aad9ea9c1f4a13ffe3c4cb383f402b5
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2024-02-29 22:07:26 -0500

    Link to api key

--- modules/ApiKeyReader.js
+++ modules/ApiKeyReader.js
@@ -1,20 +1,29 @@
 function displayApiInput(valueCallback) {
-    const apiInputContainer = document.createElement("div");
-    apiInputContainer.classList.add("apiInputContainer");
-    apiInputContainer.textContent = "Paste your OpenAI API key";
+  const apiInputContainer = document.createElement("div");
+  apiInputContainer.classList.add("apiInputContainer");
+  apiInputContainer.textContent = "Paste your OpenAI ";
+
+  const link = document.createElement("a");
+  link.textContent = "API Key";
+  link.href = "https://platform.openai.com/api-keys";
+  link.setAttribute("target", "_blank");
+  apiInputContainer.appendChild(link);
+
+  const apiInput = document.createElement("input");
+  apiInput.classList.add("apiInput");
+  apiInputContainer.appendChild(apiInput);
+
   
-    const apiInput = document.createElement("input");
-    apiInput.classList.add("apiInput");
-    apiInputContainer.appendChild(apiInput);
-  
-    document.body.appendChild(apiInputContainer);
-  
-    apiInput.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        valueCallback(apiInput.value);
-  
-        document.body.removeChild(apiInputContainer);
-      }
-    });
-  }
+
+
+  document.body.appendChild(apiInputContainer);
+
+  apiInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      valueCallback(apiInput.value);
+
+      document.body.removeChild(apiInputContainer);
+    }
+  });
+}
 export default displayApiInput;
